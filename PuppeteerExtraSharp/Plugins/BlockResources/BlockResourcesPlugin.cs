feb7d82b7b8b76e087f2337cd83f6856484fbfc0
Коваленко Олег <oleg26578@gmail.com>
2020-10-25 16:53:51 +0300

    Added plugin

---
+++ PuppeteerExtraSharp/Plugins/BlockResources/BlockResourcesPlugin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace PuppeteerExtraSharp.Plugins.BlockResources
+{
+    public class BlockResourcesPlugin : IPuppeteerExtraPlugin
+    {
+        public readonly List<BlockRule> BlockResources = new List<BlockRule>();
+        public string GetName() => "block-resources";
+
+        public BlockResourcesPlugin(IEnumerable<ResourceType> blockResources = null)
+        {
+            if (blockResources != null)
+                AddRule(builder => builder.BlockedResources(blockResources.ToArray()));
+        }
+
+        public BlockRule AddRule(Action<ResourcesBlockBuilder> builderAction)
+        {
+            var builder = new ResourcesBlockBuilder();
+            builderAction(builder);
+
+            var rule = builder.Build();
+            this.BlockResources.Add(builder.Build());
+
+            return rule;
+        }
+
+        public BlockResourcesPlugin RemoveRule(BlockRule rule)
+        {
+            BlockResources.Remove(rule);
+            return this;
+        }
+
+
+        public async Task OnPageCreated(Page page)
+        {
+            await page.SetRequestInterceptionAsync(true);
+            page.Request += (sender, args) => OnPageRequest(page, args);
+
+        }
+
+
+        private async void OnPageRequest(Page sender, RequestEventArgs e)
+        {
+            if (BlockResources.Any(rule => rule.IsRequestBlocked(sender, e.Request)))
+            {
+                await e.Request.AbortAsync();
+                return;
+            }
+
+            await e.Request.ContinueAsync();
+        }
+
+
+        public void BeforeLaunch(LaunchOptions options)
+        {
+            options.Args = options.Args.Append("--site-per-process").Append("--disable-features=IsolateOrigins").ToArray();
+        }
+
+        public List<PluginRequirements> Requirements { get; set; }
+        public ICollection<IPuppeteerExtraPlugin> Dependencies { get; set; }
+    }
+}
---
+++ PuppeteerExtraSharp/Plugins/BlockResources/BlockRule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using PuppeteerSharp;
+
+namespace PuppeteerExtraSharp.Plugins.BlockResources
+{
+    public class BlockRule
+    {
+        public string SitePattern;
+        public Page Page;
+        public HashSet<ResourceType> ResourceType = new HashSet<ResourceType>();
+
+        internal BlockRule()
+        {
+
+        }
+
+        public bool IsRequestBlocked(Page fromPage, Request request)
+        {
+            if (!IsResourcesBlocked(request.ResourceType))
+                return false;
+
+            return IsSiteBlocked(request.Url) || IsPageBlocked(fromPage);
+        }
+
+
+        public bool IsPageBlocked(Page page)
+        {
+            return Page != null && page.Equals(Page);
+        }
+
+        public bool IsSiteBlocked(string siteUrl)
+        {
+            return !string.IsNullOrWhiteSpace(SitePattern) && Regex.IsMatch(siteUrl, SitePattern);
+        }
+
+        public bool IsResourcesBlocked(ResourceType resource)
+        {
+            return ResourceType.Contains(resource);
+        }
+    }
+}
