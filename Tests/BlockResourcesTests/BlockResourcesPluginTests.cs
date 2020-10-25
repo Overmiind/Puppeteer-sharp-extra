7b124b34c0bdf925bd04c69773fc53287e57f205
Коваленко Олег <oleg26578@gmail.com>
2020-10-25 16:55:51 +0300

    Tests for block resources

---
+++ Tests/BlockResourcesTests/BlockResourcesPluginTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using PuppeteerExtraSharp.Plugins.BlockResources;
+using PuppeteerSharp;
+using Xunit;
+
+namespace Extra.Tests.BlockResourcesTests
+{
+    public class BlockResourcesPluginTests : BrowserDefault
+    {
+        public BlockResourcesPluginTests()
+        {
+
+        }
+
+        [Fact]
+        public void RuleForResources()
+        {
+            var plugin = new BlockResourcesPlugin();
+            var rule = plugin.AddRule(builder => builder.BlockedResources(ResourceType.Document));
+            Assert.NotEmpty(plugin.BlockResources);
+            Assert.Contains(ResourceType.Document, plugin.BlockResources[0].ResourceType);
+        }
+
+
+        [Fact]
+        public async Task RuleForPage()
+        {
+
+            var plugin = new BlockResourcesPlugin();
+            var browser = await base.LaunchWithPluginAsync(plugin);
+            var actualPage = (await browser.PagesAsync())[0];
+            var otherPage = await browser.NewPageAsync();
+            var rule = plugin.AddRule(builder => builder.BlockedResources(ResourceType.Document).OnlyForPage(actualPage));
+            
+            Assert.True(rule.IsPageBlocked(actualPage));
+            Assert.False(rule.IsPageBlocked(otherPage));
+        }
+
+        [Fact]
+        public void RuleForUrl()
+        {
+            var plugin = new BlockResourcesPlugin();
+            var rule = plugin.AddRule(builder => builder.ForUrl("google"));
+
+            Assert.True(rule.IsSiteBlocked("http://google.com"));
+            Assert.True(rule.IsSiteBlocked("http://google.kz"));
+            Assert.True(rule.IsSiteBlocked("https://googleeee.com"));
+        }
+    }
+}
