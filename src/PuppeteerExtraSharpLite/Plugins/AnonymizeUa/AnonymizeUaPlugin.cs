1493ce16ab8c36fc9f78a456ae635f9e1da7424d
David Shnayder <dusrdev@gmail.com>
2025-08-03 14:55:37 +0300

    Use source gen regex

--- src/PuppeteerExtraSharpLite/Plugins/AnonymizeUa/AnonymizeUaPlugin.cs
+++ src/PuppeteerExtraSharpLite/Plugins/AnonymizeUa/AnonymizeUaPlugin.cs
@@ -4,25 +4,29 @@ using PuppeteerSharp;
 
 namespace PuppeteerExtraSharpLite.Plugins.AnonymizeUa;
 
-public class AnonymizeUaPlugin : PuppeteerExtraPlugin {
+public partial class AnonymizeUaPlugin : PuppeteerExtraPlugin {
     public AnonymizeUaPlugin() : base("anonymize-ua") {
     }
 
-    private Func<string, string> _customAction;
+    private Func<string, string>? _customAction;
+
     public void CustomizeUa(Func<string, string> uaAction) {
         _customAction = uaAction;
     }
 
     public override async Task OnPageCreated(IPage page) {
-        var ua = await page.Browser.GetUserAgentAsync();
+        string ua = await page.Browser.GetUserAgentAsync();
         ua = ua.Replace("HeadlessChrome", "Chrome");
 
-        var regex = new Regex(@"/\(([^)]+)\)/");
-        ua = regex.Replace(ua, "(Windows NT 10.0; Win64; x64)");
+        ua = UserAgentRegex().Replace(ua, "(Windows NT 10.0; Win64; x64)");
 
-        if (_customAction != null)
+        if (_customAction != null) {
             ua = _customAction(ua);
+        }
 
         await page.SetUserAgentAsync(ua);
     }
+
+    [GeneratedRegex(@"/\(([^)]+)\)/")]
+    private static partial Regex UserAgentRegex();
 }
