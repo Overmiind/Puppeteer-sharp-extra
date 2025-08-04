e504ac7b28ed377c96d7406bb5c25f57393d6755
David Shnayder <dusrdev@gmail.com>
2025-08-04 12:51:26 +0300

    Sync callsite and signature to remove double string allocation

--- src/PuppeteerExtraSharpLite/Plugins/ExtraStealth/Utils.cs
+++ src/PuppeteerExtraSharpLite/Plugins/ExtraStealth/Utils.cs
@@ -3,9 +3,9 @@ using PuppeteerSharp;
 namespace PuppeteerExtraSharpLite.Plugins.ExtraStealth;
 
 internal static class Utils {
-    public static Task EvaluateOnNewPage(IPage page, ReadOnlySpan<char> script, params object[] args) {
+    public static Task EvaluateOnNewPage(IPage page, string script, params object[] args) {
         if (!page.IsClosed) {
-            return page.EvaluateFunctionOnNewDocumentAsync(script.ToString(), args);
+            return page.EvaluateFunctionOnNewDocumentAsync(script, args);
         }
 
         return Task.CompletedTask;
