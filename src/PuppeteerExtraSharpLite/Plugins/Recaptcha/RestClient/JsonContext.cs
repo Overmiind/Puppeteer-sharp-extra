1b5291df2d11b7078d7483fcd7e9c3360c509ac9
David Shnayder <dusrdev@gmail.com>
2025-08-04 11:03:41 +0300

    Added JsonElement to serialize generic json

--- src/PuppeteerExtraSharpLite/Plugins/Recaptcha/RestClient/JsonContext.cs
+++ src/PuppeteerExtraSharpLite/Plugins/Recaptcha/RestClient/JsonContext.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 using PuppeteerExtraSharpLite.Plugins.Recaptcha.Provider._2Captcha.Models;
@@ -12,4 +13,5 @@ namespace PuppeteerExtraSharpLite.Plugins.Recaptcha.RestClient;
 [JsonSerializable(typeof(RequestForResultTask))]
 [JsonSerializable(typeof(TaskResultModel))]
 [JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(JsonElement))]
 internal partial class JsonContext : JsonSerializerContext { }
