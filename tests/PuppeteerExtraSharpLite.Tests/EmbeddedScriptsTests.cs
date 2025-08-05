32b3d06a9bac0ad5fa1f6eb5a5b8a007e5a402a3
David Shnayder <dusrdev@gmail.com>
2025-08-05 11:05:47 +0300

    Updated test to new structure

--- tests/PuppeteerExtraSharpLite.Tests/EmbeddedScriptsTests.cs
+++ tests/PuppeteerExtraSharpLite.Tests/EmbeddedScriptsTests.cs
@@ -1,35 +1,32 @@
-using System.Reflection;
 using System.Text.RegularExpressions;
 
 using PuppeteerExtraSharpLite.Tests.Utils;
 
-using Scripts = PuppeteerExtraSharpLite.Plugins.EmbeddedScripts.CS.Scripts;
-
 namespace PuppeteerExtraSharpLite.Tests;
 
 public partial class EmbeddedScriptsTests {
-	public static readonly TheoryData<string, string> EnterRecaptchaCallBack = new() {
-		{ $"{nameof(Scripts.ChromeApp)}.js", Scripts.ChromeApp.ToString() },
-		{ $"{nameof(Scripts.Codec)}.js", Scripts.Codec.ToString() },
-		{ $"{nameof(Scripts.ContentWindow)}.js", Scripts.ContentWindow.ToString() },
-		{ $"{nameof(Scripts.EnterRecaptchaCallBack)}.js", Scripts.EnterRecaptchaCallBack.ToString() },
-		{ $"{nameof(Scripts.HardwareConcurrency)}.js", Scripts.HardwareConcurrency.ToString() },
-		{ $"{nameof(Scripts.Language)}.js", Scripts.Language.ToString() },
-		{ $"{nameof(Scripts.LoadTimes)}.js", Scripts.LoadTimes.ToString() },
-		{ $"{nameof(Scripts.Outdimensions)}.js", Scripts.Outdimensions.ToString() },
-		{ $"{nameof(Scripts.Permissions)}.js", Scripts.Permissions.ToString() },
-		{ $"{nameof(Scripts.Plugin)}.js", Scripts.Plugin.ToString() },
-		{ $"{nameof(Scripts.Runtime)}.js", Scripts.Runtime.ToString() },
-		{ $"{nameof(Scripts.SCI)}.js", Scripts.SCI.ToString() },
-		{ $"{nameof(Scripts.Stacktrace)}.js", Scripts.Stacktrace.ToString() },
-		{ $"{nameof(Scripts.Utils)}.js", Scripts.Utils.ToString() },
-		{ $"{nameof(Scripts.Vendor)}.js", Scripts.Vendor.ToString() },
-		{ $"{nameof(Scripts.WebDriver)}.js", Scripts.WebDriver.ToString() },
-		{ $"{nameof(Scripts.WebGL)}.js", Scripts.WebGL.ToString() }
+	public static readonly TheoryData<string, string> ScriptNames = new() {
+		{ $"{nameof(Scripts.ChromeApp)}.js", Scripts.ChromeApp },
+		{ $"{nameof(Scripts.Codec)}.js", Scripts.Codec },
+		{ $"{nameof(Scripts.ContentWindow)}.js", Scripts.ContentWindow },
+		{ $"{nameof(Scripts.EnterRecaptchaCallBack)}.js", Scripts.EnterRecaptchaCallBack },
+		{ $"{nameof(Scripts.HardwareConcurrency)}.js", Scripts.HardwareConcurrency },
+		{ $"{nameof(Scripts.Language)}.js", Scripts.Language },
+		{ $"{nameof(Scripts.LoadTimes)}.js", Scripts.LoadTimes },
+		{ $"{nameof(Scripts.Outdimensions)}.js", Scripts.Outdimensions },
+		{ $"{nameof(Scripts.Permissions)}.js", Scripts.Permissions },
+		{ $"{nameof(Scripts.Plugin)}.js", Scripts.Plugin },
+		{ $"{nameof(Scripts.Runtime)}.js", Scripts.Runtime },
+		{ $"{nameof(Scripts.SCI)}.js", Scripts.SCI },
+		{ $"{nameof(Scripts.Stacktrace)}.js", Scripts.Stacktrace },
+		{ $"{nameof(Scripts.Utils)}.js", Scripts.Utils },
+		{ $"{nameof(Scripts.Vendor)}.js", Scripts.Vendor },
+		{ $"{nameof(Scripts.WebDriver)}.js", Scripts.WebDriver },
+		{ $"{nameof(Scripts.WebGL)}.js", Scripts.WebGL }
 	};
 
 	[Theory]
-	[MemberData(nameof(EnterRecaptchaCallBack))]
+	[MemberData(nameof(ScriptNames))]
 	public void EnsureScriptConsistency(string fileNameActual, string expected) {
 		var actual = Helper.GetScriptContent(fileNameActual);
 
