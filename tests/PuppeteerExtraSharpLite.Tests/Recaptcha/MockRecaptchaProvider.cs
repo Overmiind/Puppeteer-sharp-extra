89e1ab275b1d4e2242ef278bb61f90b51201f661
David Shnayder <dusrdev@gmail.com>
2025-08-07 15:07:05 +0300

    Removed tests that test themselves

--- tests/PuppeteerExtraSharpLite.Tests/Recaptcha/MockRecaptchaProvider.cs
+++
@@ -1,9 +0,0 @@
-using PuppeteerExtraSharpLite.Plugins.Recaptcha.Provider;
-
-namespace PuppeteerExtraSharpLite.Tests.Recaptcha;
-
-public class MockRecaptchaProvider : IRecaptchaProvider {
-    public Task<string> GetSolutionAsync(string key, string pageUrl, string? proxyStr = null, CancellationToken token = default) {
-        return Task.FromResult("MOCK_CAPTCHA_TOKEN");
-    }
-}
