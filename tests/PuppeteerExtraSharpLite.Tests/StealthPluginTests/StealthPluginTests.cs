e9df2a53c95ac0f766c1f7b616f3141e80f6ce5e
David Shnayder <dusrdev@gmail.com>
2025-08-09 16:20:39 +0300

    Added more tests to cover standard evasions

--- tests/PuppeteerExtraSharpLite.Tests/StealthPluginTests/StealthPluginTests.cs
+++ tests/PuppeteerExtraSharpLite.Tests/StealthPluginTests/StealthPluginTests.cs
@@ -1,4 +1,5 @@
-﻿using PuppeteerExtraSharpLite.Plugins.Stealth;
+﻿using PuppeteerExtraSharpLite.Plugins.Recaptcha;
+using PuppeteerExtraSharpLite.Plugins.Stealth;
 
 using PuppeteerSharp;
 
@@ -43,5 +44,59 @@ public partial class StealthPluginTests {
             FullPage = true,
             Type = ScreenshotType.Png,
         });
+    }
+
+    [Fact]
+    public void Stealth_Plugin_StandardEvasionContracts_Names_Should_MatchTypes() {
+        var evasions = StealthPlugin.StandardEvasionsContracts;
+
+        foreach (var evasion in evasions) {
+            var instance = evasion.Factory();
+            Assert.Equal(evasion.PluginName, instance.Name);
+        }
+    }
+
+    [Fact]
+    public void Stealth_Plugin_GetStandardEvasions_Creates_AllStandardEvasions_NoOverrides() {
+        var evasions = StealthPlugin.StandardEvasionsContracts;
+
+        var instances = StealthPlugin.GetStandardEvasions();
+
+        Assert.Equal(evasions.Length, instances.Length);
+    }
+
+    [Fact]
+    public void Stealth_Plugin_GetStandardEvasions_Creates_AllStandardEvasions_AndOverrides() {
+        var evasions = StealthPlugin.StandardEvasionsContracts;
+
+        int defaultConcurrencyLevel = new HardwareConcurrencyPlugin().ConcurrencyLevel;
+        const int customConcurrencyLevel = 16;
+
+        Assert.NotEqual(customConcurrencyLevel, defaultConcurrencyLevel);
+
+        var p = new HardwareConcurrencyPlugin(customConcurrencyLevel);
+
+        var instances = StealthPlugin.GetStandardEvasions(p);
+
+        Assert.Equal(evasions.Length, instances.Length); // override should not create extra
+
+        var match = instances.SingleOrDefault(i => i is HardwareConcurrencyPlugin);
+
+        Assert.NotNull(match); // If match is null, override didn't work correctly
+
+        // Ensure found match has current parameters (override args)
+        Assert.Equal(customConcurrencyLevel, (match as HardwareConcurrencyPlugin)!.ConcurrencyLevel);
+    }
+
+    [Fact]
+    public void Stealth_Plugin_GetStandardEvasions_Creates_AllStandardEvasions_UnrelatedOverrides() {
+        var evasions = StealthPlugin.StandardEvasionsContracts;
+
+        var p = new RecaptchaPlugin(); // unrelated to stealth
+
+        var instances = StealthPlugin.GetStandardEvasions(p);
+
+        // unrelated should not override, so count should be bigger than 1
+        Assert.Equal(evasions.Length + 1, instances.Length);
     }
 }
