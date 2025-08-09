350103cd921ecef33fa1ca315ac9e099735c8915
David Shnayder <dusrdev@gmail.com>
2025-08-09 16:16:39 +0300

    Added mock provider for testing purposes

--- src/PuppeteerExtraSharpLite/Plugins/Recaptcha/Provider/IRecaptchaProvider.cs
+++ src/PuppeteerExtraSharpLite/Plugins/Recaptcha/Provider/IRecaptchaProvider.cs
@@ -3,3 +3,9 @@
 public interface IRecaptchaProvider {
     Task<string> GetSolutionAsync(string key, string pageUrl, string proxyStr = "", CancellationToken token = default);
 }
+
+internal sealed class InvalidRecaptchaProvider : IRecaptchaProvider {
+    public Task<string> GetSolutionAsync(string key, string pageUrl, string proxyStr = "", CancellationToken token = default) {
+        throw new NotImplementedException();
+    }
+}
