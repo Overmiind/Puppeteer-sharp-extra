aa7ca7ed74e31f4937b3978e4b0980dd97fda67b
David Shnayder <dusrdev@gmail.com>
2025-08-11 12:14:19 +0300

    Commented out anti-captcha models

--- src/PuppeteerSharpToolkit/JsonContext.cs
+++ src/PuppeteerSharpToolkit/JsonContext.cs
@@ -1,11 +1,11 @@
 using System.Text.Json.Serialization;
 
-using PuppeteerSharpToolkit.Plugins.Recaptcha.AntiCaptcha;
+// using PuppeteerSharpToolkit.Plugins.Recaptcha.AntiCaptcha;
 using PuppeteerSharpToolkit.Plugins.Recaptcha.TwoCaptcha;
 
 namespace PuppeteerSharpToolkit;
 
-[JsonSerializable(typeof(AntiCaptchaTaskResult))]
-[JsonSerializable(typeof(AntiCaptchaTaskResultFull))]
+// [JsonSerializable(typeof(AntiCaptchaTaskResult))]
+// [JsonSerializable(typeof(AntiCaptchaTaskResultFull))]
 [JsonSerializable(typeof(TwoCaptchaResponse))]
 internal partial class JsonContext : JsonSerializerContext;
